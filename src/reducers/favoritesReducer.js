3381b1e15523d2aa73d0ae3bfefcdd4fc0d3191d
rpdecks <rpdecks@gmail.com>
2020-07-04 10:54:25 -0400

    favorites working

--- src/reducers/favoritesReducer.js
+++ src/reducers/favoritesReducer.js
@@ -3,12 +3,12 @@ export default function favoritesReducer(state = {}, action) {
     case 'FAVORITE_USER':
         return {
             ...state,
-            employerFavorites: [...state.employerFavorites, action.favorite]
+            userFavorites: [...state.userFavorites, action.favorite]
         }
     case 'UNFAVORITE_USER':
         return {
             ...state,   
-            employerFavorites: [...state.employerFavorites.filter(fav => fav.id !== action.favoriteInstanceId)]
+            userFavorites: [...state.userFavorites.filter(fav => fav.id !== action.favoriteInstanceId)]
         }
     case 'STORE_USER_FAVORITES':
         return {
