ac88aa302d136fda5bdf6631058817ddad596f90
rpdecks <rpdecks@gmail.com>
2021-01-15 07:56:08 -0500

    add thunk to package.json

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import { connect } from 'react-redux'
-import { withRouter } from 'react-router';
-import { Link } from 'react-router-dom'
-import { Button } from 'react-bootstrap'
-import styled from 'styled-components'
-import { FaBabyCarriage } from 'react-icons/fa'
-import { fetchData, loginFetch } from '../actions/fetches'
+import React from "react";
+import { connect } from "react-redux";
+import { withRouter } from "react-router";
+import { Link } from "react-router-dom";
+import { Button } from "react-bootstrap";
+import styled from "styled-components";
+import { FaBabyCarriage } from "react-icons/fa";
+import { fetchData, loginFetch } from "../actions/fetches";
 
-const Styles = styled.div ` 
+const Styles = styled.div`
   margin-top: 5rem;
-  font-family: 'Roboto', sans-serif;
+  font-family: "Roboto", sans-serif;
 
   .header-text {
-    font-family: 'Pacifico', cursive;
+    font-family: "Pacifico", cursive;
     text-align: center;
     font-size: large;
     color: #757575;
@@ -20,15 +20,15 @@ const Styles = styled.div `
   .brand {
     text-align: center;
     font-size: xx-large;
-    color: #0097A7;
-    font-family: 'Pacifico', cursive;
+    color: #0097a7;
+    font-family: "Pacifico", cursive;
   }
   .instructions {
     text-align: center;
     font-size: medium;
     color: #757575;
-    margin-top: 1rem; 
-    margin-bottom: 1rem; 
+    margin-top: 1rem;
+    margin-bottom: 1rem;
   }
   .form-div {
     margin-top: 2 rem;
@@ -37,99 +37,140 @@ const Styles = styled.div `
     color: #757575;
   }
   a:link {
-      color: #00BCD4;
+    color: #00bcd4;
   }
   a:visited {
-      color: #757575;
+    color: #757575;
   }
-`
-
+`;
 
 class LoginForm extends React.Component {
   state = {
-    email: '',
-    password: ''
-  }
+    email: "",
+    password: "",
+  };
 
-  handleChange = e => {
+  handleChange = (e) => {
     this.setState({ [e.target.name]: e.target.value });
-  }
+  };
 
-  handleLogin = token => {
-    localStorage.setItem('auth_token', token);
-    localStorage.setItem('userType', this.props.userType);
-    this.props.setLoginStatus(true)
-  }
+  handleLogin = (token) => {
+    localStorage.setItem("auth_token", token);
+    localStorage.setItem("userType", this.props.userType);
+    this.props.setLoginStatus(true);
+  };
 
-  login = e => {
+  login = (e) => {
     e.preventDefault();
-
-    console.log(this.props);
 
     const userObj = {
       [this.props.userType]: {
         email: this.state.email,
-        password: this.state.password
-      }
-    }
+        password: this.state.password,
+      },
+    };
 
-    this.props.loginFetch(this.props.userType, userObj)
-    if (this.props.userType === 'employer') {
-      this.props.history.push('/pending-jobs')
-    } else { this.props.history.push('/jobs') }
-    this.props.fetchData(this.props.userType)
-    e.target.reset()
-  }
+    this.props.loginFetch(this.props.userType, userObj);
+    if (this.props.userType === "employer") {
+      this.props.history.push("/pending-jobs");
+    } else {
+      this.props.history.push("/jobs");
+    }
+    this.props.fetchData(this.props.userType);
+    e.target.reset();
+  };
 
   render() {
     return (
       <Styles>
-        <div className='header-text'>
-          Welcome to
+        <div className="header-text">Welcome to</div>
+        <div className="brand">
+          <FaBabyCarriage />
+          Babysitter!
+          <FaBabyCarriage />
         </div>
-        <div className='brand'>
-        <FaBabyCarriage />
-        Babysitter!
-        <FaBabyCarriage />
+        <div className="instructions">
+          Login below to your{" "}
+          <em>
+            <b>{this.props.userType}</b>
+          </em>{" "}
+          account below:
         </div>
-        <div className='instructions'>
-          Login below to your <em><b>{this.props.userType}</b></em> account below:
-        </div>
-        <div className='form-div'>
+        <div className="form-div">
           <form onSubmit={(e) => this.login(e)}>
-          <label>Email:</label><br />
-          <input name="email" label="Email" onChange={e => this.handleChange(e)} /><br />
-          <label>Password:</label><br />
-          <input name="password" type="password" label="Password" onChange={e => this.handleChange(e)} /><br />
-          <Button type="submit" style={{ backgroundColor: "#0097A7", color: 'white', border: '0', marginLeft: '5px', marginTop: '1rem', marginBottom: '0.5rem' }} >
+            <label>Email:</label>
+            <br />
+            <input
+              name="email"
+              label="Email"
+              onChange={(e) => this.handleChange(e)}
+            />
+            <br />
+            <label>Password:</label>
+            <br />
+            <input
+              name="password"
+              type="password"
+              label="Password"
+              onChange={(e) => this.handleChange(e)}
+            />
+            <br />
+            <Button
+              type="submit"
+              style={{
+                backgroundColor: "#0097A7",
+                color: "white",
+                border: "0",
+                marginLeft: "5px",
+                marginTop: "1rem",
+                marginBottom: "0.5rem",
+              }}
+            >
               Submit
-          </Button>
-          <Button onClick={() => this.props.setUserType(null)} style={{ backgroundColor: "#0097A7", color: 'white', border: '0', marginLeft: '5px', marginTop: '1rem', marginBottom: '0.5rem' }} >
+            </Button>
+            <Button
+              onClick={() => this.props.setUserType(null)}
+              style={{
+                backgroundColor: "#0097A7",
+                color: "white",
+                border: "0",
+                marginLeft: "5px",
+                marginTop: "1rem",
+                marginBottom: "0.5rem",
+              }}
+            >
               Cancel
-          </Button><br />
-          <Link to="/signup" onClick={() => this.props.setSigningUp(true)}>Create an account</Link>
+            </Button>
+            <br />
+            <Link to="/signup" onClick={() => this.props.setSigningUp(true)}>
+              Create an account
+            </Link>
           </form>
-          </div>
+        </div>
       </Styles>
-    )
+    );
   }
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
     user: state.userReducer.user,
     userType: state.userReducer.userType,
     isLoggedIn: state.userReducer.isLoggedIn,
-  }
-}
+  };
+};
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch) => {
   return {
-    setSigningUp: (condition) => dispatch({ type: 'SETTING_SIGNING_UP', signingUp: condition }),
-    setUserType: (value) => dispatch({ type: 'SET_USER_TYPE', userType: value}),
+    setSigningUp: (condition) =>
+      dispatch({ type: "SETTING_SIGNING_UP", signingUp: condition }),
+    setUserType: (value) =>
+      dispatch({ type: "SET_USER_TYPE", userType: value }),
     fetchData: (userType) => dispatch(fetchData(userType)),
     loginFetch: (userType, userObj) => dispatch(loginFetch(userType, userObj)),
-  }
-}
+  };
+};
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(LoginForm));
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(LoginForm)
+);
