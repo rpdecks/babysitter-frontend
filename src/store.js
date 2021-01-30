29fa8c0f6ce5e11c60e424ad05e556ef50cc8ee7
rpdecks <rpdecks@gmail.com>
2021-01-29 22:34:06 -0500

    Fix broken login
    Redux devtools extension was improperly installed in `store.js`. Fix
    properly returns a function as arg. White screen of death on attempt to
    load site is fixed.
    
    Login would hang on a loading screen. Login fetch was originally
    abstracted into actions.js file. The login fetch was not completing
    before the app's attempt to hydrate. Login fetch was moved back to the
    login component and `fetchData` is now called on a successful response.

--- src/store.js
+++ src/store.js
@@ -1,10 +1,10 @@
-import { combineReducers, compose, createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk'
-import jobReducer from './reducers/jobReducer';
-import userReducer from './reducers/userReducer';
-import reviewsReducer from './reducers/reviewsReducer';
-import favoritesReducer from './reducers/favoritesReducer';
-import uiReducer from './reducers/uiReducer';
+import { combineReducers, compose, createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import jobReducer from "./reducers/jobReducer";
+import userReducer from "./reducers/userReducer";
+import reviewsReducer from "./reducers/reviewsReducer";
+import favoritesReducer from "./reducers/favoritesReducer";
+import uiReducer from "./reducers/uiReducer";
 
 const appReducer = combineReducers({
   uiReducer,
@@ -16,13 +16,18 @@ const appReducer = combineReducers({
 
 const rootReducer = (state, action) => {
   // when a logout action is dispatched it will reset redux state
-  if (action.type === 'USER_LOGGED_OUT') {
+  if (action.type === "USER_LOGGED_OUT") {
     state = undefined;
   }
-  return appReducer(state, action)
-}
+  return appReducer(state, action);
+};
 
 export default createStore(
   rootReducer,
-  compose( applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__({trace: true}))
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__
+      ? window.__REDUX_DEVTOOLS_EXTENSION__({ trace: true })
+      : (f) => f
+  )
 );
